d8746116573513b5e9f3b26a6245eee0cd1e9890
夜花櫻 <yanasakuranight@gmail.com>
2023-03-27 22:36:14 +0800

    modify default text

--- src/main/java/TinyYana/YanaCommunityBot/ConfigFile/BotConfig.java
+++ src/main/java/TinyYana/YanaCommunityBot/ConfigFile/BotConfig.java
@@ -74,7 +74,12 @@ public class BotConfig {
         set("Welcome_Message_Enable", true);
         set("Welcome_Message_Channel_ID", 859089963181670410L);
         set("Welcome_Message_Title", ":tada: 歡迎加入彼岸花社群");
-        set("Welcome_Message_Description", "這裡是一個可以讓大家聊，\\n「ACG相關話題」的社群，\\n:thumbsup: 社群推薦文作者也會不定時推薦好看的動畫作品給大家。\\n\\n:speaking_head: 當然你也可以純閒聊，\\n與這裡的大家一起閒話家常，\\n相信你可以在這邊認識更多新朋友。");
+        set("Welcome_Message_Description",
+                """
+                        這裡是一個可以讓大家聊，「ACG相關話題」的社群，:thumbsup:
+                        社群推薦文作者也會不定時推薦好看的動畫作品給大家。:speaking_head:
+                        當然你也可以純閒聊，與這裡的大家一起閒話家常，相信你可以在這邊認識更多新朋友。
+                        """);
         set("Welcome_Message_Color", "YELLOW");
 
         set("Dynamic_Voice_Channel_Enable", true);
